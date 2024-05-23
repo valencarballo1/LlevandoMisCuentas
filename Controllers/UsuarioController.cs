75e8d0dd2151b37496128e257749ea0adaab81a7
Valentin Carballo <valentincarballo99@gmail.com>
2024-05-23 15:17:54 -0300

    login creado

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,61 @@
+﻿using Business;
+using Data;
+using Data.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LlevandoMisCuentas.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private UsuarioBusiness _UsuarioBusiness;
+
+        public UsuarioController()
+        {
+            this._UsuarioBusiness = new UsuarioBusiness();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult Login(Usuario usuario)
+        {
+            Usuario usuarioExiste = _UsuarioBusiness.Existe(usuario);
+
+            if (usuarioExiste != null)
+            {
+                HttpCookie cookie = new HttpCookie("UsuarioSesion");
+                cookie["Id"] = usuarioExiste.Id.ToString(); // Puedes almacenar más información según tus necesidades
+                Response.Cookies.Add(cookie);
+            }
+            return Json(usuarioExiste, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public JsonResult CerrarSesion()
+        {
+            if (Request.Cookies["UsuarioSesion"] != null)
+            {
+                var cookie = new HttpCookie("UsuarioSesion")
+                {
+                    Expires = DateTime.Now.AddDays(-1)
+                };
+
+                Response.Cookies.Add(cookie);
+            }
+            return Json(new { success = true }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult Registrarme(UsuarioRegistroDTO usuario)
+        {
+            bool registro = _UsuarioBusiness.Registrarme(usuario);
+            return Json(registro);
+        }
+    }
+}
